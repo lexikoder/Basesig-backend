37a47f1230b106b7e27d4c6d6a6d8ef4a0d38597
ifeanyi uzor <uzorifeanyi1000@gmail.com>
2025-10-27 12:49:43 +0100

    update

--- utils/tryCatch.js
+++ utils/tryCatch.js
@@ -1,33 +1,33 @@
-// const tryCatch = (controller) => {
-//  return async(req,res,next) =>{
-//    try{
-//      await controller(req,res)
-//    }catch(e){
-//     console.log(e)
-//        next(e)
-//    }
-//    }
-// }
+const tryCatch = (controller) => {
+ return async(req,res,next) =>{
+   try{
+     await controller(req,res)
+   }catch(e){
+    console.log(e)
+       next(e)
+   }
+   }
+}
 
-// module.exports = tryCatch
+module.exports = tryCatch
 
-const tryCatch = (controller, timeoutMs = 30000) => {
-  return async (req, res, next) => {
-    const timer = setTimeout(() => {
-      if (!res.headersSent) {
-        res.status(408).json({ error: "Request timed out" });
-      }
-    }, timeoutMs);
+// const tryCatch = (controller, timeoutMs = 30000) => {
+//   return async (req, res, next) => {
+//     const timer = setTimeout(() => {
+//       if (!res.headersSent) {
+//         res.status(408).json({ error: "Request timed out" });
+//       }
+//     }, timeoutMs);
 
-    try {
-      await controller(req, res, next);
-    } catch (err) {
-      console.error(err);
-      if (!res.headersSent) next(err);
-    } finally {
-      clearTimeout(timer);
-    }
-  };
-};
+//     try {
+//       await controller(req, res, next);
+//     } catch (err) {
+//       console.error(err);
+//       if (!res.headersSent) next(err);
+//     } finally {
+//       clearTimeout(timer);
+//     }
+//   };
+// };
 
 
